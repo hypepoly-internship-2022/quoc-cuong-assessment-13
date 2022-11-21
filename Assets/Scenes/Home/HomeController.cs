61c8d5b9684493a83617131c5c86b6711531dedb
chimpuchim <96745654+chimpuchim@users.noreply.github.com>
2022-11-21 10:38:36 +0700

    +build scene ssview home and loading

---
+++ Assets/Scenes/Home/HomeController.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SS.View;
+
+public class HomeController : Controller
+{
+    public const string HOME_SCENE_NAME = "Home";
+
+    public override string SceneName()
+    {
+        return HOME_SCENE_NAME;
+    }
+}
